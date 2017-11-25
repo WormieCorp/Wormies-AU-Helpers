6e8cc064bf544e3b3a35d4bbda1c12b00884c570
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-25 19:50:39 +0100

    Added 'Get-RedirectedUrl' helper

---
+++ Wormies-AU-Helpers/public/Get-RedirectedUrl.ps1
@@ -0,0 +1,27 @@
+﻿function Get-RedirectedUrl {
+    param(
+        [Parameter(Mandatory = $true)]
+        [uri]$url,
+        [uri]$referer
+    )
+
+    $req = [System.Net.WebRequest]::CreateDefault($url)
+    if ($referer) {
+        $req.Referer = $referer
+    }
+
+    $resp = $req.GetResponse()
+
+    if ($resp -and $resp.ResponseUri.OriginalString -ne $url) {
+        Write-Verbose "Found redirected url '$($resp.ResponseUri)"
+        $result = $resp.ResponseUri.OriginalString
+    }
+    else {
+        Write-Warning "No redirected url was found, returning given url."
+        $result = $url
+    }
+
+    $resp.Dispose()
+
+    return $result
+}
