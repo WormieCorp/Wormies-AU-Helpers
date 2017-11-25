ab7b9fd95ba636e0232c87bbdcfc2831c2c90ce6
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-25 20:01:33 +0100

    Added ability to upload coverage to codecov

--- test.ps1
+++ test.ps1
@@ -21,9 +21,20 @@ if ($Pester) {
     $testResultsFile = "$buildDir/TestResults.xml"
     if ($CodeCoverage) {
         $files = @(ls $PSScriptRoot/Wormies-AU-Helpers/* -Filter *.ps1 -Recurse | % FullName)
-        Invoke-Pester -Tag $Tag -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru -CodeCoverage $files
+        if (Test-Path Env:\APPVEYOR -and (Get-Command Export-CodeCovIoJson -ea 0)) {
+            $coverageFile = "$buildDir/coverage.xml"
+            $res = Invoke-Pester -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru -CodeCoverage $files -CodeCoverageOutputFile "$coverageFile"
+            (New-Object "System.Net.WebClient").UploadFile("https://ci.appveyor.com/testresults/nunit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path $testResultsFile))
+            Export-CodeCovIoJson -CodeCoverage $res.CodeCoverage -RepoRoot $PSScriptRoot -Path "$buildDir/coverage.json"
+            if ($res.FailedCount -gt 0) {
+                throw "$($res.FailedCount) tests failed"
+            }
+        }
+        else {
+            $res = Invoke-Pester -Tag $Tag -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru -CodeCoverage $files
+        }
     }
     else {
-        Invoke-Pester -Tag $Tag -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
+        $res = Invoke-Pester -Tag $Tag -OutputFormat NUnitXml -OutputFile $testResultsFile -PassThru
     }
 }
