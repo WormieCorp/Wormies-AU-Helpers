822bf1282658a9d91507a5a4f68bd71171c90963
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-26 12:18:51 +0100

    Added basic documentation to 'Get-RedirectedUrl'

--- Wormies-AU-Helpers/public/Get-RedirectedUrl.ps1
+++ Wormies-AU-Helpers/public/Get-RedirectedUrl.ps1
@@ -1,4 +1,23 @@
-﻿function Get-RedirectedUrl {
+﻿<#
+.SYNOPSIS
+    Aquire the url that is being redirected to when using the passed url parameter
+
+.DESCRIPTION
+    When a website only supplies a url to the latest version, but that url is redirected
+    to a different url to aquire the actual binary file.
+    Then this function can be used to simplify that aquiral.
+
+.PARAMETER url
+    The url to check for redirection
+
+.PARAMETER referer
+    An optional parameter to use when a website requires the referer header to
+    be used
+
+.OUTPUTS
+    The redirected url when one is found, otherwise returns the same url that was passed.
+#>
+function Get-RedirectedUrl {
     param(
         [Parameter(Mandatory = $true)]
         [uri]$url,
