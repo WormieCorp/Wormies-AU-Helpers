b17cd0b029ba7b115a6b5c53e5aca4713ba6a87b
Kim J. Nordmo <kim.nordmo@gmail.com>
2017-11-26 19:24:45 +0100

    (maint) Added 'Update-Metadata' helper

---
+++ Wormies-AU-Helpers/public/Update-Metadata.ps1
@@ -0,0 +1,51 @@
+﻿$ErrorActionPreference = 'Stop'
+
+<#
+.SYNOPSIS
+Updates the metadata nuspec file with the specified information.
+
+.DESCRIPTION
+When a key and value is specified, update the metadata element with the specified key
+and the corresponding value in the specified NuspecFile.
+
+.PARAMETER key
+The element that should be updated in the metadata section.
+
+.PARAMETER value
+The value to update with.
+
+.PARAMETER NuspecFile
+The metadata/nuspec file to update
+
+.EXAMPLE
+`Update-Metadata -key releaseNotes -value "https://github.com/majkinetor/AU/releases/latest"`
+
+.EXAMPLE
+`Update-Metadata -key releaseNotes -value "https://github.com/majkinetor/AU/releases/latest" -NuspecFile ".\package.nuspec"`
+
+.NOTES
+Will throw an exception if the specified key doesn't exist in the nuspec file.
+#>
+function Update-Metadata {
+    param(
+        [Parameter(Mandatory = $true)]
+        [string]$key,
+        [Parameter(Mandatory = $true)]
+        [string]$value,
+        [ValidateScript( { Test-Path $_ })]
+        [string]$NuspecFile = ".\*.nuspec"
+    )
+
+    $NuspecFile = Resolve-Path $NuspecFile
+
+    $nu = New-Object xml
+    $nu.PSBase.PreserveWhitespace = $true
+    $nu.Load($NuspecFile)
+    if ($nu.package.metadata."$key") {
+        $nu.package.metadata."$key" = "$value"
+    }
+    else {
+        throw "$key does not exist on the metadata element in the nuspec file"
+    }
+    $nu.Save($NuspecFile)
+}
---
+++ tests/public/Update-Metadata.Tests.ps1
@@ -0,0 +1,47 @@
+﻿Remove-Module wormies-au-helpers -ea ignore
+Import-Module "$PSScriptRoot/../../Wormies-AU-Helpers"
+
+Describe "Update-Metadata" {
+    $nuspecFile = "$PSScriptRoot\test.nuspec"
+    BeforeEach {
+        $xml = @'
+<?xml version="1.0" encoding="utf-8"?>
+<!-- Do not remove this test for UTF-8: if “Ω” doesn’t appear as greek uppercase omega letter enclosed in quotation marks, you should use an editor that supports UTF-8, not this one. -->
+<package xmlns="http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd">
+    <metadata>
+        <id>ValidNuspec</id>
+        <title>Valid Test Nuspec</title>
+        <version>1.0.3</version>
+    </metadata>
+</package>
+'@
+        $utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($false)
+        [System.IO.File]::WriteAllText($nuspecFile, $xml, $utf8NoBomEncoding)
+    }
+
+    AfterEach {
+        Remove-Item $nuspecFile -Force
+    }
+
+    It "Can update id with value 'new-id'" {
+        Update-Metadata -key 'id' -value 'new-id' -NuspecFile $nuspecFile
+
+        $nuspecFile | Should -FileContentMatch "\<id\>new\-id\<\/id\>"
+    }
+
+    It "Can update version with value '6.2.12'" {
+        Update-Metadata -key "version" -value "6.2.12" -NuspecFile $nuspecFile
+
+        $nuspecFile | Should -FileContentMatch "\<version\>6\.2\.12\<\/version\>"
+    }
+
+    It "Can update title with value 'Let me test out changing the title'" {
+        Update-Metadata -key "title" -value "Let me test out changing the title" -NuspecFile $nuspecFile
+
+        $nuspecFile | Should -FileContentMatch "\<title\>Let me test out changing the title\<\/title\>"
+    }
+
+    It "Throws exception when item doesn't exist" {
+        { Update-Metadata -key "rlsNotes" -value "I'm the new item" -NuspecFile $nuspecFile } | Should -Throw "rlsNotes does not exist on the metadata element in the nuspec file"
+    }
+}
