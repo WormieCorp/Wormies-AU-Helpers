62752abaa74badace12e21898cddc626a6577fe8
AdmiringWorm <kim.nordmo@gmail.com>
2018-05-17 14:03:55 +0200

    (maint) Added helper to parse the version info

---
+++ Wormies-AU-Helpers/private/Update-VersionInformation.ps1
@@ -0,0 +1,37 @@
+﻿function Update-VersionInformation {
+    param(
+        [hashtable]$Package,
+        [switch]$UseFileVersion
+    )
+
+
+    function GetVersion {
+        param($path)
+
+        $version = if ($UseFileVersion) {
+            Get-Item $path | % { $_.VersionInfo.FileVersion }
+        }
+        else {
+            Get-Item $path | % { $_.VersionInfo.ProductVersion }
+        }
+
+        return $version -replace ',','.'
+    }
+    $has32Bit = $has64Bit = $false
+
+    if ($Package["FilePath32"] -and (Test-Path $Package["FilePath32"])) {
+        $Package["Version32"] = GetVersion $Package["FilePath32"]
+        $has32Bit = $true
+    }
+
+    if ($Package["FilePath64"] -and (Test-Path $Package["FilePath64"])) {
+        $Package["Version64"] = GetVersion $Package["FilePath64"]
+        $has64Bit = $true
+    }
+
+    $versionMatches = (!$has32Bit -and $has64Bit) `
+        -or ($has32Bit -and !$has64Bit) `
+        -or $Package["Version32"] -eq $Package["Version64"]
+
+    return $versionMatches
+}
---
+++ tests/private/Update-VersionInformation.Tests.ps1
@@ -0,0 +1,36 @@
+﻿Remove-Module wormies-au-helpers -ea ignore
+Import-Module "$PSScriptRoot/../../Wormies-AU-Helpers"
+
+Describe "Versioning" {
+    $url32 = 'https://www.7-zip.org/a/7z1805.exe'
+    $url64 = 'https://www.7-zip.org/a/7z1805-x64.exe'
+    $fileName32 = "$env:TEMP\7z1805.exe"
+    $fileName64 = "$env:TEMP\7z1805-x64.exe"
+    if (!(Test-Path $fileName32)) { iwr -Uri $url32 -OutFile $fileName32 -UseBasicParsing }
+    if (!(Test-Path $fileName64)) { iwr -Uri $url64 -OutFile $fileName64 -UseBasicParsing }
+
+    It "Should get correct 32bit version from executable" {
+        $package = @{ FilePath32 = $fileName32 }
+        $result = Update-VersionInformation $package
+
+        $package["Version32"] | Should -Be "18.05"
+        $result | Should -Be $true
+    }
+
+    It "Should get correct 64bit version from executable" {
+        $package = @{ FilePath64 = $fileName32 }
+        $result = Update-VersionInformation $package
+
+        $package["Version64"] | Should -Be "18.05"
+        $result | Should -Be $true
+    }
+
+    It "Should get correct 32 and 64bit version from executable" {
+        $package = @{ FilePath32 = $fileName32; FilePath64 = $fileName64 }
+        $result = Update-VersionInformation $package
+
+        $package["Version32"] | Should -Be "18.05"
+        $package["Version64"] | Should -Be "18.05"
+        $result | Should -Be $true
+    }
+}
