342849448e36b6846347fc2c1a8daacde49995f3
AdmiringWorm <kim.nordmo@gmail.com>
2018-05-17 14:02:43 +0200

    (maint) Added helper to get remote headers

---
+++ Wormies-AU-Helpers/private/Get-RemoteHeaders.ps1
@@ -0,0 +1,20 @@
+﻿function Get-RemoteHeaders {
+    param([string]$uri, [string[]]$OnlyUseHeaders = $null)
+
+    $req = [System.Net.WebRequest]::CreateDefault($uri)
+
+    $resp = $req.GetResponse()
+
+    $headers = @{}
+
+    $resp.Headers.AllKeys | ? {
+        $key = $_
+        (!$OnlyUseHeaders -or ($OnlyUseHeaders | ? { $_ -eq $key }))
+    } | % {
+        $value = ([System.Net.WebResponse]$resp).Headers.GetValues($_)
+        if ($value.Count -eq 1) { $headers[$_] = $value | select -first 1 }
+        elseif ($value.Count -gt 1) { $headers[$_] = $value }
+    }
+
+    return $headers
+}
---
+++ tests/private/Get-RemoteHeaders.Tests.ps1
@@ -0,0 +1,31 @@
+﻿Remove-Module wormies-au-helpers -ea ignore
+Import-Module "$PSScriptRoot/../../Wormies-AU-Helpers"
+
+Describe "Networking" {
+    It "Should get all headers if not overridden" {
+        $expectedHeaders = @{
+            'accept-ranges'  = 'bytes'
+            'content-length' = 5433008
+            'etag'           = '0x8D5B46C90EED410'
+            'content-md5'    = '5BeA69heva5evcojv495sQ=='
+            # There is alot more headers, but this should be enough for a test
+        }
+
+        $actualHeaders = Get-RemoteHeaders -uri 'https://dist.nuget.org/win-x86-commandline/v4.7.0/nuget.exe'
+
+        $expectedHeaders.Keys | % {
+            $key = $_
+            $value = $expectedHeaders[$key]
+            $actualHeaders.ContainsKey($key) | Should -Be $true
+            $actualHeaders[$key] | Should -Be $value
+        }
+    }
+
+    It "Should only return headers we want" {
+        $expectedHeader = "etag"
+
+        $actualHeaders = Get-RemoteHeaders -uri 'https://dist.nuget.org/win-x86-commandline/v4.7.0/nuget.exe' -OnlyUseHeaders @('etag')
+
+        $actualHeaders.Keys | % { $_ | Should -Be $expectedHeader }
+    }
+}
