fbe96849012bbcf4b15b2875997cad04b249f60b
AdmiringWorm <kim.nordmo@gmail.com>
2018-05-17 14:03:29 +0200

    (maint) Added helper to download files

---
+++ Wormies-AU-Helpers/private/Invoke-FileDownload.ps1
@@ -0,0 +1,49 @@
+﻿function Invoke-FileDownload {
+    param(
+        [uri]$URL32,
+        [uri]$URL64,
+        [hashtable]$Options = @{},
+        [switch]$WhatIf
+    )
+
+    $filePath32 = [System.IO.Path]::GetTempFileName()
+    $filePath64 = [System.IO.Path]::GetTempFileName()
+
+    $result = @{}
+
+    $existingModule = Get-Module "chocolateyInstaller" -ea 0
+    if (!$existingModule) {
+        if ($WhatIf) {
+            Write-Host "Would import the chocolateyInstaller module..."
+        }
+        else {
+            Import-Module $env:ChocolateyInstall\helpers\chocolateyInstaller.psm1
+        }
+    }
+
+    if (![string]::IsNullOrEmpty($URL32)) {
+        if ($WhatIf) {
+            Write-Host "Would download 32bit binary file..."
+        }
+        else {
+            Write-Host "Downloading 32bit binary file..."
+            Get-WebFile -url $URL32 -fileName $filePath32 -Options $Options
+        }
+        $result["FilePath32"] = $filePath32
+        $result["URL32"] = $URL32
+    }
+
+    if (![string]::IsNullOrEmpty($URL64)) {
+        if ($WhatIf) {
+            Write-Host "Would download 64bit binary file..."
+        }
+        else {
+            Write-Host "Downloading 64bit binary file..."
+            Get-WebFile -url $URL64 -fileName $filePath64 -Options $Options
+        }
+        $result["FilePath64"] = $filePath64
+        $result["URL64"] = $URL64
+    }
+
+    return $result
+}
---
+++ tests/private/Invoke-FileDownload.Tests.ps1
@@ -0,0 +1,38 @@
+﻿Remove-Module wormies-au-helpers -ea ignore
+Import-Module "$PSScriptRoot/../../Wormies-AU-Helpers"
+
+Describe "Downloading" {
+    It "Can download 32bit binary file" {
+        $result = Invoke-FileDownload -URL32 'https://www.7-zip.org/a/7z1805.exe'
+
+        $result["FilePath32"] | Should -Not -Be $null
+
+        $itemName = Get-Item $result["FilePath32"] | % { $_.VersionInfo.ProductName }
+
+        $itemName | Should -Be '7-Zip'
+    }
+
+    It "Should return 32bit info when download 32bit file" {
+        $expectedUrl = "https://www.7-zip.org/a/7z1805.exe"
+        $result = Invoke-FileDownload -URL32 $expectedUrl -WhatIf
+        $result["URL32"] | Should -Be $expectedUrl
+        $result["FilePath32"] | Should -not -Be $null
+    }
+
+    It "Can download 64bit binary file" {
+        $result = Invoke-FileDownload -URL64 'https://www.7-zip.org/a/7z1805-x64.exe'
+
+        $result["FilePath64"] | Should -Not -Be $null
+
+        $itemName = Get-Item $result["FilePath64"] | % { $_.VersionInfo.ProductName }
+
+        $itemName | Should -Be '7-Zip'
+    }
+
+    It "Should return 64bit info when download 64bit file" {
+        $expectedUrl = "https://www.7-zip.org/a/7z1805-x64.exe"
+        $result = Invoke-FileDownload -URL64 $expectedUrl -WhatIf
+        $result["URL64"] | Should -Be $expectedUrl
+        $result["FilePath64"] | Should -not -Be $null
+    }
+}
