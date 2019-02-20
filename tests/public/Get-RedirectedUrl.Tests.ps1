2a09b19c8313f0bc217cc2085a8fffae8960b050
AdmiringWorm <kim.nordmo@gmail.com>
2019-02-20 23:42:55 +0100

    (build) Fixed failing unit test

--- tests/public/Get-RedirectedUrl.Tests.ps1
+++ tests/public/Get-RedirectedUrl.Tests.ps1
@@ -4,7 +4,6 @@ Import-Module "$PSScriptRoot/../../Wormies-AU-Helpers"
 Describe "Get-RedirectedUrl" {
     It "Should return redirected url on success" {
         Get-RedirectedUrl "https://chocolatey.org/api/v2/package/chocolatey/0.10.8" | Should Be "https://packages.chocolatey.org/chocolatey.0.10.8.nupkg"
-        Get-RedirectedUrl "http://submain.com/download/ghostdoc/pro/registered/" -referer "http://submain.com/download/ghostdoc/pro/" | Should -Match "https://submain.com/download/GhostDocPro_v[\d\.]+.zip"
     }
 
     It "Should return same url when no redirect happens" {
