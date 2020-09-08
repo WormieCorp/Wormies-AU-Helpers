2ae0038ed58e03e4eff69c683c627ff02d5e4056
RB <RedBaron2@users.noreply.github.com>
2020-09-08 14:32:58 -0700

    update per review

--- Wormies-AU-Helpers/public/Update-Metadata.ps1
+++ Wormies-AU-Helpers/public/Update-Metadata.ps1
@@ -7,13 +7,7 @@ Updates the metadata nuspec file with the specified information.
 .DESCRIPTION
 When a key and value is specified, update the metadata element with the specified key
 and the corresponding value in the specified NuspecFile.
-Updating a file/dependency key must have at least one attribute defined.
-( EX: <file src="" /> -or- <dependency id="" /> )
-The updating of file/dependency key uses a pipe delimiter instead of comma. 
-When updating file/dependency key that has multiple file/dependency key requires
-a change value. This value is counted from one. The value must follow the values of
-the attributes being changed. The Attributes for File are src, target, & exclude.
-The Attributes for Dependency are id, version, exclude, & include.
+Singlular metadata elements are the only ones changed at this time.
 
 .PARAMETER key
 The element that should be updated in the metadata section.
@@ -43,30 +37,21 @@ Update-Metadata -data @{ dependency = 'kb2919355|1.0.20160915' }
 @{ dependency = 'kb2919355|1.0.20160915' } | Update-Metadata
 
 .EXAMPLE
-This is an example of changing the id and include attributes at the second dependency key
-Without changing the values for attributes version or exclude (if present)
-Update-Metadata -data @{ dependency = 'kb2919355||1.0.20160915,2' }
-- or -
-@{ dependency = 'kb2919355||1.0.20160915,2' } | Update-Metadata
-
-.EXAMPLE
-This is an example of changing the src and target attributes for the file key
-Update-Metadata -data @{ file = 'tools\**|tools' }
+This is an example of changing the src and target attributes
+Update-Metadata -data @{ file = 'tools\**,tools' }
 - or -
 @{ file = 'tools\**|tools' } | Update-Metadata
 
 .EXAMPLE
-This is an example of changing the src and exclude attributes at the second file key
-Without changing the values for attribute target
-Update-Metadata -data @{ file = 'tools\**||tools,2' }
-- or -
-@{ file = 'tools\**||tools,2' } | Update-Metadata
+This is an example of changing the file src and target attributes for the first file element in the nuspec file.
+If only one file element is found the change value is omitted.
+@{ file = 'tools\**|tools,1' } | Update-Metadata
 
 .NOTES
     Will now show a warning if the specified key doesn't exist in the nuspec file.
-    Will now show a warning when the change value for file/dependency key is greater than currently in the nuspec file.
-    Will now show a warning when file/dependency key doesn't have any defined attributes.
-    Will now show a warning when the change value has been omitted due to nodes not allowing change at that key.
+    Will now show a warning when change for file/dependency key is greater than currently in the nuspec file.
+    Will now show a warning when change has been omitted due to nodes not allowing change at that key.
+    Will now show a warning when file/dependency doesn't have any attributes defined to be updated.
 
     While the parameter `NuspecFile` accepts globbing patterns,
     it is expected to only match a single file.
@@ -81,10 +66,11 @@ function Update-Metadata {
         [Parameter(Mandatory = $true, ParameterSetName = "Single")]
         [string]$value,
         [Parameter(Mandatory = $true, ParameterSetName = "Multiple", ValueFromPipeline = $true)]
-        [hashtable]$data = [ordered] @{$key = $value},
+        [hashtable]$data = @{$key = $value},
         [ValidateScript( { Test-Path $_ })]
         [SupportsWildcards()]
         [string]$NuspecFile = ".\*.nuspec"
+
     )
 
     $NuspecFile = Resolve-Path $NuspecFile
@@ -92,24 +78,46 @@ function Update-Metadata {
     $nu = New-Object xml
     $nu.PSBase.PreserveWhitespace = $true
     $nu.Load($NuspecFile)
+    $omitted = $true
     $data.Keys | ForEach-Object {
         switch -Regex ($_) {
             '^(file)$' {
-                $metaData = "files"; $NodeGroup = $nu.package.$metaData
+                $metaData = "files"
+                $NodeGroup = $nu.package.$metaData
                 $NodeData,[int]$change = $data[$_] -split (",")
-                $NodeCount = $nu.package.$metaData.ChildNodes.Count; $src,$target,$exclude = $NodeData -split ("\|")
-                $NodeAttributes = [ordered] @{"src" = $src;"target" = $target;"exclude" = $exclude}
+                $NodeCount = $nu.package.$metaData.ChildNodes.Count
+                $src,$target,$exclude = $NodeData -split ("\|")
+                $NodeAttributes = [ordered] @{
+                                              "src"     = $src
+                                              "target"  = $target
+                                              "exclude" = $exclude
+                                            }
                 $change = @{$true="0";$false=($change - 1)}[ ([string]::IsNullOrEmpty($change)) ]
-                if ($NodeCount -eq 3) { $NodeGroup = $NodeGroup."$_"; $omitted = $true } else { $NodeGroup = $NodeGroup.$_[$change] }
+                if ($NodeCount -eq 3) {
+                    $NodeGroup = $NodeGroup."$_"
+                } else {
+                    $NodeGroup = $NodeGroup.$_[$change]
+                }
             }
             '^(dependency)$' {
-                $MetaNode = $_ -replace("y","ies"); $metaData = "metadata"
+                $MetaNode = $_ -replace("y","ies")
+                $metaData = "metadata"
                 $NodeData,[int]$change = $data[$_] -split (",")
-                $NodeGroup = $nu.package.$metaData.$MetaNode; $NodeCount = $nu.package.$metaData.$MetaNode.ChildNodes.Count
+                $NodeGroup = $nu.package.$metaData.$MetaNode
+                $NodeCount = $nu.package.$metaData.$MetaNode.ChildNodes.Count
                 $id,$version,$include,$exclude = $NodeData -split ("\|")
-                $NodeAttributes = [ordered] @{"id" = $id;"version" = $version;"include" = $include;"exclude" = $exclude}
+                $NodeAttributes = [ordered] @{
+                                             "id"      = $id
+                                             "version" = $version
+                                             "include" = $include
+                                             "exclude" = $exclude
+                                            }
                 $change = @{$true="0";$false=($change - 1)}[ ([string]::IsNullOrEmpty($change)) ]
-                if ($NodeCount -eq 3) { $NodeGroup = $NodeGroup."$_"; $omitted = $true } else { $NodeGroup = $NodeGroup.$_[$change] }
+                if ($NodeCount -eq 3) {
+                    $NodeGroup = $NodeGroup."$_"
+                } else {
+                    $NodeGroup = $NodeGroup.$_[$change]
+                }
             }
             default {
                 if ( $nu.package.metadata."$_" ) {
